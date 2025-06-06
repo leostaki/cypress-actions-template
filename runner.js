b1c19bdaafaf657cdc6335a9ead6124ba970385f
leostaki <leostaki@gmail.com>
2025-06-06 18:21:01 -0300

    Update token runner.js

--- runner.js
+++ runner.js
@@ -1,7 +1,7 @@
 const cypress = require('cypress')
 const tesults = require('cypress-tesults-reporter');
 
-const TOKEN = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6ImM3NmVjOWQ2LTRlMGUtNGVlZC05MThmLWI5ZTEwZjE5YTEwNC0xNzQ5MjM4MTc5MzgxIiwiZXhwIjo0MTAyNDQ0ODAwMDAwLCJ2ZXIiOiIwIiwic2VzIjoiZmQ0NzM1NmUtYjg3ZC00ZTBmLTg3YzEtZmI0YjFhMjkzYTI4IiwidHlwZSI6InQifQ.jeSCmRNDrdnQaz2xCcS7BQ6ekPAHWEGwkFE4DYbhkr4'
+const TOKEN = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6ImM3NmVjOWQ2LTRlMGUtNGVlZC05MThmLWI5ZTEwZjE5YTEwNC0xNzQ5MjM4MTc5MzgxIiwiZXhwIjo0MTAyNDQ0ODAwMDAwLCJ2ZXIiOiIwIiwic2VzIjoiYjNkZTNjMzQtN2ZjNC00NTFiLWFmMGYtMzFjMDZhMjM5M2MxIiwidHlwZSI6InQifQ.GrLSy--272G_IHB5DG1geKixhNvfpBRFZR6miHyfQck'
 cypress.run({
   // specs to run here
 })
