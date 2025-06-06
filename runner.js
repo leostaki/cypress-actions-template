bf6c2c4e34093e68ef8edfb5f5cf02c7a3365c51
leostaki <leostaki@gmail.com>
2025-06-06 16:39:47 -0300

    Create runner.js
    integrando testes com tsults

---
+++ runner.js
@@ -0,0 +1,16 @@
+const cypress = require('cypress')
+const tesults = require('cypress-tesults-reporter');
+
+const TOKEN: 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6ImM3NmVjOWQ2LTRlMGUtNGVlZC05MThmLWI5ZTEwZjE5YTEwNC0xNzQ5MjM4MTc5MzgxIiwiZXhwIjo0MTAyNDQ0ODAwMDAwLCJ2ZXIiOiIwIiwic2VzIjoiZmQ0NzM1NmUtYjg3ZC00ZTBmLTg3YzEtZmI0YjFhMjkzYTI4IiwidHlwZSI6InQifQ.jeSCmRNDrdnQaz2xCcS7BQ6ekPAHWEGwkFE4DYbhkr4'
+cypress.run({
+  // specs to run here
+})
+.then((results) => {
+  const args = {
+    target: TOKEN,
+  }
+  tesults.results(results, args);
+})
+.catch((err) => {
+ console.error(err)
+})
